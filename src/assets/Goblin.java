126132c5f4fef03c3879f4cbdbb1c0fdb915a164
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2021-11-10 17:42:34 -0500

    updated goblin

--- src/assets/Goblin.java
+++ src/assets/Goblin.java
@@ -3,6 +3,7 @@ package assets;
 public class Goblin extends Actor {
 
     public Goblin() {
+        // Goblin base constructor
         System.out.println("New Goblin was created!!!");
         this.setName("Goblin");
         this.setAttack(5);
