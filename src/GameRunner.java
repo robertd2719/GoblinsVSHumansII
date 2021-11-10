6eb1fa3b3c65ee6479e5bee9ed181a7533d3eb0f
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2021-11-10 11:49:11 -0500

    updated the ability to move items inside the map...up,down,left,right now functioning correctly

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,19 +1,10 @@
 import assets.Human;
-import assets.Goblin;
 import board.GameBoard;
 
 public class GameRunner {
     public static void main(String[] args) {
         Human human = new Human();
-        Goblin goblin = new Goblin();
-        GameBoard board = new GameBoard(10,10);
-        board.placeItem(goblin,0,0);
-        board.placeItem(human,1,1);
-        board.moveItemLeft(human,1);
-        board.moveItemRight(human,2);
-        board.moveItemRight(goblin,5);
-        board.moveItemRight(goblin,15);
-        board.displayBoard();
+        GameBoard board = new GameBoard(12,12);
 
     }
 }
--- src/board/BoardTester.java
+++ src/board/BoardTester.java
@@ -5,6 +5,7 @@ import assets.Human;
 
 public class BoardTester {
     public static void main(String[] args) {
+        GameBoard board1 = new GameBoard();
         GameBoard board = new GameBoard(12,12);
         board.displayBoard();
     }
