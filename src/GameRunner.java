39184f173c7600f6615779bf03a97ff136a91ba9
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2021-11-14 06:30:18 -0500

    various input and output updates

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,36 +1,16 @@
-import assets.*;
+import assets.Actor;
+import assets.Goblin;
+import assets.Human;
 import board.GameBoard;
-import item.*;
-import board.MoveDirection;
+
+import java.util.Scanner;
 
 public class GameRunner {
     public static void main(String[] args) {
-        Human human = new Human();
-        Goblin goblin = new Goblin();
-        GameBoard board = new GameBoard(15,20);
-        board.placeActor(human,1,1);
-        board.placeActor(goblin,5,5);
-        board.displayBoard();
-        board.trackEnemy(goblin,human);
-        board.displayBoard();
-        board.trackEnemy(goblin,human);
-        board.displayBoard();
-        board.trackEnemy(goblin,human);
-        board.displayBoard();
-        // Attack routine will trigger
-        Actor died = goblin.attack(human);
-        if (died.getName().equals("Human")){
-            System.out.println("You Died Game Over");
-            board.removeItem(died);
-        }
-        else if (died.getName().equals("Goblin"))
-            board.removeItem(died);
-        board.displayBoard();
-
-
-
+        MainLoop();
     }
-    public static void defaultMenu(){
+
+    public static void displayMenu() {
         System.out.println("\t\t***** Main Menu ****");
         System.out.println("\t\t\t (M)ove");
         System.out.println("\t\t\t (A)ttack");
@@ -39,13 +19,46 @@ public class GameRunner {
 
     }
 
-    public static void tesStats(Actor item){
+    public static void tesStats(Actor item) {
         System.out.println("\n***** STATS ******");
-        System.out.println("Name: \t\t"+item.getName());
-        System.out.println("Health: \t"+item.getHealth());
-        System.out.println("Attack: \t"+item.getAttack());
-        System.out.println("Armor: \t"+item.getArmorClass());
+        System.out.println("Name: \t\t" + item.getName());
+        System.out.println("Health: \t" + item.getHealth());
+        System.out.println("Attack: \t" + item.getAttack());
+        System.out.println("Armor: \t" + item.getArmorClass());
         System.out.println("***** + ******");
+    }
+
+    public static void MainLoop() {
+        Human human = new Human();
+        Goblin goblin = new Goblin();
+        GameBoard board = new GameBoard(15, 20);
+        board.placeActor(human, 1, 1);
+        board.placeActor(goblin, 5, 5);
+        board.displayBoard();
+        while (true) {
+            displayOptions();
+            var reply = getPlayerInput();
+            System.out.println("player chose: " + reply);
+        }
+    }
+
+    public static String getPlayerInput() {
+        String playerInput = "";
+        Scanner in = new Scanner(System.in);
+        try {
+            playerInput = in.next();
+        } catch (Exception err) {
+            System.out.println("...something went wrong!");
+            System.out.println(err.getMessage());
+        }
+        return playerInput;
+    }
+
+    public static void displayOptions() {
+        System.out.println("Player, please proceed");
+        System.out.println("(1) Move");
+        System.out.println("(2) Use item");
+        System.out.println("(3) Attack");
     }
 
 
