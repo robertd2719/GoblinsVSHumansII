83a86e8c9b192598da3b16fe14d5f80d8fc55279
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2021-11-15 03:32:44 -0500

    updated scoring protocol to reflect the players score in a file and to evaluate if the player does in fact have a high score....need to add to endgame procedure

--- src/gameRunner/GameRunner.java
+++ src/gameRunner/GameRunner.java
@@ -10,6 +10,10 @@ import item.Shield;
 import item.Sword;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -211,13 +215,49 @@ public class GameRunner {
         System.out.println("\t---------Game Over-----------");
     }
 
-    public static void clearScreen() {
-        try {
-            Runtime.getRuntime().exec("cls");
-        } catch (IOException err) {
-            System.out.println("Sorry unable to clear screen!!!");
+    // If the file does not exist we will attempt to create it here
+    public static void createFile(Path path) {
+        if (Files.exists(path)) {
+            System.out.println("High Score file exists");
+        } else {
+            try {
+                Files.createFile(path);
+                System.out.println("high_score file was created!!!");
+            } catch (Exception err) {
+                System.out.println("Unable to ope file");
+                System.out.println(err.getMessage());
+            }
         }
     }
+
+    // Here we will write to the file itself
+    public static void writeFile(Path path) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(" -- Pleas enter player name: ");
+        var name = in.next();
+        System.out.print(" -- Please enter player score: ");
+        var score = in.next();
+        try {
+            Files.writeString(path, name + " " + score + "\n", StandardOpenOption.APPEND);
+        } catch (IOException err) {
+            System.out.println(err.getMessage());
+            System.out.println("Unable to write to file");
+        }
+    }
+
+    public static List readFile(Path path) {
+        try {
+            var input = Files.readAllLines(path);
+            return input;
+        } catch (IOException err) {
+            System.out.println(err.getMessage());
+            System.out.println("Unable to read file");
+        }
+        return null;
+    }
+
+
+
 
     public static void pause(int secs) {
         try {
